d8c7b0de84bec88e1714985d003eb6e7a41440ab
Matthew Short <darkhelmet125@yahoo.com>
2013-11-23 23:01:11 -0800

    Initial Commit

---
+++ CPSC_486_Assign_1_initWithData_tut/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPSC_486_Assign_1_initWithData_tut
+//
+//  Created by William Short on 11/23/13.
+//  Copyright (c) 2013 William Short. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
